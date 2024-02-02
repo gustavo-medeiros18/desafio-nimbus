25a6cc22f6c4913786dadb066923c12276140f02
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-02-02 10:28:46 -0300

    :sparkles: feat: Adiciona conexão com o mysql e teste de query.

--- src/get-damage-summary-by-date/repository.js
+++ src/get-damage-summary-by-date/repository.js
@@ -1,5 +1,40 @@
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 module.exports = {
-  async execute() {
-    // TODO: acesso ao banco
+  execute: async function () {
+    const connection = mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      port: process.env.MYSQL_PORT,
+    });
+
+    connection.connect((err) => {
+      if (err) {
+        console.error("Error connecting to MySQL database:", err);
+        return;
+      }
+      console.log("Connected to MySQL database");
+    });
+
+    connection.query("SELECT * FROM alerts", (err, results, fields) => {
+      if (err) {
+        console.error("Error querying MySQL database:", err);
+        return;
+      }
+      console.log("Results from MySQL database:", results);
+    });
+
+    connection.end((err) => {
+      if (err) {
+        console.error("Error disconnecting from MySQL database:", err);
+        return;
+      }
+      console.log("Disconnected from MySQL database");
+    });
   },
 };
