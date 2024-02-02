d3634a2030bb54a02992620105f2f03e9254336e
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-02-02 11:34:17 -0300

    :sparkles: feat: Adiciona chamada e retorno do controller no endpoint.

--- src/get-damage-summary-by-date/endpoint.js
+++ src/get-damage-summary-by-date/endpoint.js
@@ -3,5 +3,8 @@ const controller = require("./controller");
 module.exports = {
   execute() {
     // TODO: tratamento da requisição e resposta do caso de uso
+    const alerts = controller.execute();
+
+    return alerts;
   },
 };
