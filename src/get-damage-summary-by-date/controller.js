7e8b39d0776717f29733613b903f9aa077e68647
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-02-03 10:54:22 -0300

    :recycle: refact: Altera o nome do objeto resultante para alerts

--- src/get-damage-summary-by-date/controller.js
+++ src/get-damage-summary-by-date/controller.js
@@ -16,7 +16,7 @@ module.exports = {
   async execute(dateStart, dateEnd) {
     const dbAlerts = await repository.execute(dateStart, dateEnd);
 
-    const dates = dbAlerts.reduce(
+    const alerts = dbAlerts.reduce(
       (result, alert) => {
         const formattedDate = formatDate(alert.date);
 
@@ -48,55 +48,15 @@ module.exports = {
       }
     );
 
-    dates.data = Object.keys(dates.dateSummary).map((date) => ({
+    alerts.data = Object.keys(alerts.dateSummary).map((date) => ({
       date,
-      avgDamage: Math.floor(dates.totalDamageByDate[date] / dates.dateSummary[date]),
-      maxDamageEvent: dates.maxDamageEventByDate[date],
-      minDamageEvent: dates.minDamageEventByDate[date],
+      avgDamage: Math.floor(alerts.totalDamageByDate[date] / alerts.dateSummary[date]),
+      maxDamageEvent: alerts.maxDamageEventByDate[date],
+      minDamageEvent: alerts.minDamageEventByDate[date],
     }));
 
     return { data: dates.data };
 
-    // return dbAlerts
-    //   .reduce((result, alert) => {
-    //     const dateAlreadySummarized = result.find(({ date }) => date === alert.date);
-    //     const {
-    //       damages: oldDamages,
-    //       maxDamageEvent: oldMaxDamageEvent,
-    //       minDamageEvent: oldMinDamageEvent,
-    //     } = { ...dateAlreadySummarized };
-    //     const date = alert.date;
-    //     const damages = (oldDamages || []).concat([alert.damage]);
-    //     let maxDamageEvent = alert;
-    //     if (oldMaxDamageEvent && oldMaxDamageEvent.damage > alert.damage) {
-    //       maxDamageEvent = oldMaxDamageEvent;
-    //     }
-    //     let minDamageEvent;
-    //     if (oldMinDamageEvent && oldMinDamageEvent.damage < alert.damage) {
-    //       minDamageEvent = oldMinDamageEvent;
-    //     }
-    //     minDamageEvent = alert;
-
-    //     if (dateAlreadySummarized) {
-    //       dateAlreadySummarized.damages = damages;
-    //       dateAlreadySummarized.maxDamageEvent = maxDamageEvent;
-    //       dateAlreadySummarized.minDamageEvent = minDamageEvent;
-    //     } else {
-    //       result.push({
-    //         date,
-    //         damages,
-    //         maxDamageEvent,
-    //         minDamageEvent,
-    //       });
-    //     }
-
-    //     return result;
-    //   }, [])
-    //   .sort((a, b) => b.date.localeCompare(a.date))
-    //   .forEach((summary) => {
-    //     summary.avgDamage = summary.damages.reduce((result, damage) => result + damage, 0);
-    //     delete summary.damages;
-    //     return summary;
-    //   });
+    return { data: alerts.data };
   },
 };
