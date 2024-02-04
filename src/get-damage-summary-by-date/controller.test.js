844c0e6f31b0c6e896036d1ffe088e13f2a5780d
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-02-04 00:28:19 -0300

    :sparkles: feat: Modifica o teste básico, considerando as propriedades que podem ser null.

--- src/get-damage-summary-by-date/controller.test.js
+++ src/get-damage-summary-by-date/controller.test.js
@@ -1,6 +1,6 @@
 const controller = require("./controller");
 
-test("should return average, maximum and minimum events daily in date range", async () => {
+it("should return average, maximum and minimum events daily in date range", async () => {
   const dateStart = "2014-01-01";
   const dateEnd = "2014-01-10";
 
@@ -18,15 +18,21 @@ test("should return average, maximum and minimum events daily in date range", as
     expect(typeof dailyResult.avgDamage).toBe("number");
 
     expect(dailyResult).toHaveProperty("maxDamageEvent");
-    expect(dailyResult.maxDamageEvent).toHaveProperty("event");
-    expect(typeof dailyResult.maxDamageEvent.event).toBe("string");
-    expect(dailyResult.maxDamageEvent).toHaveProperty("damage");
-    expect(typeof dailyResult.maxDamageEvent.damage).toBe("number");
 
-    expect(dailyResult).toHaveProperty("minDamageEvent");
-    expect(dailyResult.minDamageEvent).toHaveProperty("event");
-    expect(typeof dailyResult.minDamageEvent.event).toBe("string");
-    expect(dailyResult.minDamageEvent).toHaveProperty("damage");
-    expect(typeof dailyResult.minDamageEvent.damage).toBe("number");
+    if (dailyResult.avgDamage) {
+      expect(dailyResult.maxDamageEvent).toHaveProperty("event");
+      expect(typeof dailyResult.maxDamageEvent.event).toBe("string");
+      expect(dailyResult.maxDamageEvent).toHaveProperty("damage");
+      expect(typeof dailyResult.maxDamageEvent.damage).toBe("number");
+
+      expect(dailyResult).toHaveProperty("minDamageEvent");
+      expect(dailyResult.minDamageEvent).toHaveProperty("event");
+      expect(typeof dailyResult.minDamageEvent.event).toBe("string");
+      expect(dailyResult.minDamageEvent).toHaveProperty("damage");
+      expect(typeof dailyResult.minDamageEvent.damage).toBe("number");
+    } else {
+      expect(dailyResult.maxDamageEvent).toBe(null);
+      expect(dailyResult.minDamageEvent).toBe(null);
+    }
   });
 });
