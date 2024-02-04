bd6bb770304c35338b8b6ce853752911e2f9ed95
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-02-04 01:42:41 -0300

    :white_check_mark: test: Adiciona mais testes e organiza em suite.

--- src/get-damage-summary-by-date/controller.test.js
+++ src/get-damage-summary-by-date/controller.test.js
@@ -1,38 +1,69 @@
 const controller = require("./controller");
 
-it("should return average, maximum and minimum events daily in date range", async () => {
-  const dateStart = "2014-01-01";
-  const dateEnd = "2014-01-10";
+describe("Controller Tests", () => {
+  describe("execute function", () => {
+    it("should return average, maximum and minimum events daily in date range", async () => {
+      const dateStart = "2014-01-01";
+      const dateEnd = "2014-01-10";
 
-  const result = await controller.execute(dateStart, dateEnd);
+      const result = await controller.execute(dateStart, dateEnd);
 
-  expect(result).toHaveProperty("data");
-  expect(Array.isArray(result.data)).toBe(true);
-  expect(result.data.length).toBeGreaterThan(0);
+      expect(result).toHaveProperty("data");
+      expect(Array.isArray(result.data)).toBe(true);
+      expect(result.data.length).toBeGreaterThan(0);
 
-  result.data.forEach((dailyResult) => {
-    expect(dailyResult).toHaveProperty("date");
-    expect(dailyResult.date).toMatch(/^\d{4}-\d{2}-\d{2}$/);
+      result.data.forEach((dailyResult) => {
+        expect(dailyResult).toHaveProperty("date");
+        expect(dailyResult.date).toMatch(/^\d{4}-\d{2}-\d{2}$/);
 
-    expect(dailyResult).toHaveProperty("avgDamage");
-    expect(typeof dailyResult.avgDamage).toBe("number");
+        expect(dailyResult).toHaveProperty("avgDamage");
+        expect(typeof dailyResult.avgDamage).toBe("number");
 
-    expect(dailyResult).toHaveProperty("maxDamageEvent");
+        expect(dailyResult).toHaveProperty("maxDamageEvent");
 
-    if (dailyResult.avgDamage) {
-      expect(dailyResult.maxDamageEvent).toHaveProperty("event");
-      expect(typeof dailyResult.maxDamageEvent.event).toBe("string");
-      expect(dailyResult.maxDamageEvent).toHaveProperty("damage");
-      expect(typeof dailyResult.maxDamageEvent.damage).toBe("number");
+        if (dailyResult.avgDamage) {
+          expect(dailyResult.maxDamageEvent).toHaveProperty("event");
+          expect(typeof dailyResult.maxDamageEvent.event).toBe("string");
+          expect(dailyResult.maxDamageEvent).toHaveProperty("damage");
+          expect(typeof dailyResult.maxDamageEvent.damage).toBe("number");
 
-      expect(dailyResult).toHaveProperty("minDamageEvent");
-      expect(dailyResult.minDamageEvent).toHaveProperty("event");
-      expect(typeof dailyResult.minDamageEvent.event).toBe("string");
-      expect(dailyResult.minDamageEvent).toHaveProperty("damage");
-      expect(typeof dailyResult.minDamageEvent.damage).toBe("number");
-    } else {
-      expect(dailyResult.maxDamageEvent).toBe(null);
-      expect(dailyResult.minDamageEvent).toBe(null);
-    }
+          expect(dailyResult).toHaveProperty("minDamageEvent");
+          expect(dailyResult.minDamageEvent).toHaveProperty("event");
+          expect(typeof dailyResult.minDamageEvent.event).toBe("string");
+          expect(dailyResult.minDamageEvent).toHaveProperty("damage");
+          expect(typeof dailyResult.minDamageEvent.damage).toBe("number");
+        } else {
+          expect(dailyResult.maxDamageEvent).toBe(null);
+          expect(dailyResult.minDamageEvent).toBe(null);
+        }
+      });
+    });
+
+    it("should throw an error if dateStart is greater than dateEnd", async () => {
+      const dateStart = "2014-01-10";
+      const dateEnd = "2014-01-01";
+
+      await expect(controller.execute(dateStart, dateEnd)).rejects.toThrow(
+        "dateStart must be less than or equal to dateEnd."
+      );
+    });
+
+    it("should throw an error if dateStart is not a valid date", async () => {
+      const dateStart = "2014-01-32";
+      const dateEnd = "2014-01-01";
+
+      await expect(controller.execute(dateStart, dateEnd)).rejects.toThrow(
+        "Both dateStart and dateEnd must be in the format 'yyyy-mm-dd'."
+      );
+    });
+
+    it("should throw an error if dateEnd is not a valid date", async () => {
+      const dateStart = "2014-01-01";
+      const dateEnd = "2014-01-32";
+
+      await expect(controller.execute(dateStart, dateEnd)).rejects.toThrow(
+        "Both dateStart and dateEnd must be in the format 'yyyy-mm-dd'."
+      );
+    });
   });
 });
