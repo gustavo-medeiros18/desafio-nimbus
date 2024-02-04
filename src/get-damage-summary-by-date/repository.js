b376840605bb8f1bd21c9ba1a392ad0f62dfc9b2
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-02-04 10:42:07 -0300

    :recycle: refact: Altera o módulo repository para utilizar a conexão separada.

--- src/get-damage-summary-by-date/repository.js
+++ src/get-damage-summary-by-date/repository.js
@@ -1,29 +1,16 @@
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
-
-dotenv.config();
+const { getConnection } = require("./connection");
 
 module.exports = {
   execute: async function (dateStart, dateEnd) {
-    const connection = mysql.createConnection({
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      port: process.env.MYSQL_PORT,
-    });
-
-    connection.connect((error) => {
-      if (error) throw new Error("Error connecting to MySQL database:", error);
-    });
+    const database = getConnection();
 
     return new Promise((resolve, reject) => {
       const sqlStatement = "SELECT * FROM alerts WHERE date BETWEEN ? AND ?";
 
-      connection.query(sqlStatement, [dateStart, dateEnd], (error, results) => {
+      database.query(sqlStatement, [dateStart, dateEnd], (error, results) => {
         if (error) reject(error);
 
-        connection.end();
+        database.end();
         resolve(results);
       });
     });
