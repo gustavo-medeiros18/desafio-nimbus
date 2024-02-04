c37bc65dd22b43fdd23eacd00e0ae2ce152aa4a8
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-02-03 23:32:41 -0300

    :sparkles: feat: Adiciona rotina que inclui as datas sem alertas no array resultante.

--- src/get-damage-summary-by-date/controller.js
+++ src/get-damage-summary-by-date/controller.js
@@ -48,9 +48,26 @@ module.exports = {
       }
     );
 
+    // Preencher lacunas com datas ausentes entre dateStart e dateEnd
+    const currentDate = new Date(dateStart);
+    const endDate = new Date(dateEnd);
+    while (currentDate <= endDate) {
+      const formattedDate = formatDate(currentDate);
+      if (!alerts.dateSummary[formattedDate]) {
+        alerts.dateSummary[formattedDate] = 0;
+        alerts.totalDamageByDate[formattedDate] = null;
+        alerts.maxDamageEventByDate[formattedDate] = null;
+        alerts.minDamageEventByDate[formattedDate] = null;
+      }
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
     alerts.data = Object.keys(alerts.dateSummary).map((date) => ({
       date,
-      avgDamage: Math.floor(alerts.totalDamageByDate[date] / alerts.dateSummary[date]),
+      avgDamage:
+        alerts.totalDamageByDate[date] !== null
+          ? Math.floor(alerts.totalDamageByDate[date] / alerts.dateSummary[date])
+          : 0,
       maxDamageEvent: alerts.maxDamageEventByDate[date],
       minDamageEvent: alerts.minDamageEventByDate[date],
     }));
