9498b1992f75e62119cf0c68839302d62abcddd2
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-02-04 00:49:42 -0300

    :recycle: refact: Refatora endpoint para tratar a requisição e resposta.

--- src/get-damage-summary-by-date/endpoint.js
+++ src/get-damage-summary-by-date/endpoint.js
@@ -1,10 +1,13 @@
 const controller = require("./controller");
 
 module.exports = {
-  execute(dateStart, dateEnd) {
+  async execute(req, res) {
     // TODO: tratamento da requisição e resposta do caso de uso
-    const alerts = controller.execute(dateStart, dateEnd);
+    const dateStart = req.query.dateStart;
+    const dateEnd = req.query.dateEnd;
 
-    return alerts;
+    const alerts = await controller.execute(dateStart, dateEnd);
+
+    res.status(200).json(alerts);
   },
 };
