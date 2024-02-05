864544a4b969d1a41701c009ce75d29349f05676
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-02-05 00:57:35 -0300

    :seedling: env: Remove variável de ambiente POSTGRES_DB.

--- src/get-damage-summary-by-date/database/connection.js
+++ src/get-damage-summary-by-date/database/connection.js
@@ -9,7 +9,6 @@ module.exports = {
       host: process.env.POSTGRES_HOST,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
       port: process.env.POSTGRES_PORT,
     });
 
